18d60a01d6a7663b581297ba88005d67677d4c6a
AnuragLovesCode <anurag.neet01@gmail.com>
2025-08-24 12:26:02 +0530

    add WalletReward in user model

--- models/user.model.js
+++ models/user.model.js
@@ -68,6 +68,10 @@ const userSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+     walletReward: {
+      type: Number,
+      default: 0,
+    },
     lastCreditedDate: { type: Date },
     level: {
       type: Number,
